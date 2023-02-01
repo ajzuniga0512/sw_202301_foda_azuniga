b5ae0fb4680b2651e6325ab48f23bbfe700601c5
Alejandro Zuniga <84946172+ajzuniga0512@users.noreply.github.com>
2023-01-31 19:43:06 -0600

    update
    codigo para actualizar los datos de una empresa

--- src/routes/empresas/empresas.ts
+++ src/routes/empresas/empresas.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-import {Empresas} from '@libs/Empresas/Empresas';
+import {Empresas, IEmpresa} from '@libs/Empresas/Empresas';
 
 const empresasModel= new Empresas();
 
@@ -34,6 +34,59 @@ router.get('/', (_req, res)=>{
 
 router.get('/all', (_req, res)=>{
     res.status(200).json(empresasModel.getAll());
+});
+
+router.post('/new', (req, res) =>{
+    console.log("Empresas /new request body:", req.body);
+
+    const {
+        nombre="Alejandro Zuniga", 
+        status="Activo"
+    } = req.body;
+
+    const newEmpresa: IEmpresa = {
+        codigo: "",
+        nombre,
+        status,
+        created: undefined,
+        updated: undefined
+    };
+
+    if(empresasModel.add(newEmpresa)) {
+        return res.status(200).json({"created":true});
+    }
+
+    return res.status(404).json({"error": "Al agregar una nueva empresa"});
+
+});
+
+router.put('/upd/:id', (req, res)=>{
+    const { id } = req.params;
+
+    const {
+        nombre="Alejandro Zuniga", 
+        status="Activo", 
+        observacion=""
+        
+    }=req.body;
+    
+    const updateEmpresa : IEmpresa = {
+        codigo: id,
+        nombre,
+        status,
+        observacion,
+        created: undefined,
+        updated: undefined
+    };
+
+    if (empresasModel.update(updateEmpresa)) {
+        return res.status(200).json({"Se cambio": "Se actualizo la empresa"});
+    }
+
+    return res.status(404).json({"error": "Al actualizar la empresa"});
+
+
+    
 });
 
 /* router.get('/', (_req, res)=>{
